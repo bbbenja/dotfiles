1221f676474ad6299dc41afd9328a5b0f270d7d3
Benjamin Cousin <benou.cousin@gmail.com>
2013-11-07 16:45:26 +0100

    add xchat config

---
+++ install.sh
@@ -0,0 +1,24 @@
+# install.sh
+
+# install brew
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+# install git
+# install jvm
+# install intellij, mongo, glassfish, umongo, bettertouch, alfred, photoshop, illustrator, daisydisk, diffmerge, firefox, indigostudio, office, networkconnect, tweetdeck
+# configure ssh
+# create macos partition sensible à la casse
+# install yoman
+
+
+# bootsrap.sh
+./bootsrap.sh
+
+# .osx
+./.osx
+
+# .brew
+./.brew
+
+# .cask
+./.cask
