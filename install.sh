d2d1da2f525f2c38dd670d7a76259e83d6119327
Benjamin Cousin <benou.cousin@gmail.com>
2013-11-08 17:40:15 +0100

    add .glassfish

--- install.sh
+++ install.sh
@@ -4,9 +4,17 @@
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
 # install git
-# install jvm
-# install intellij, mongo, glassfish, umongo, bettertouch, alfred, photoshop, illustrator, daisydisk, diffmerge, firefox, indigostudio, office, networkconnect, tweetdeck, plug, xchat (config), jrebel
+brew install git
+
 # configure ssh
+ssh-keygen -t rsa -C "srmvision" #let id_rsa
+cat id_rsa.pub | ssh bcousin@prp-int.srm 'cat >> .ssh/authorized_keys'
+# add key to gitlab
+
+# install jvm
+
+# install intellij, mongo, glassfish, umongo, bettertouch, alfred, photoshop, illustrator, daisydisk, diffmerge, firefox, indigostudio, office, networkconnect, tweetdeck, plug, xchat (config), jrebel
+
 # create macos partition sensible à la casse
 # install yoman
 
